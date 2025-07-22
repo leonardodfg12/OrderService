86ce75d74e3dcb5b0361532c059f0b794775de05
leonardo.rodrigues <leonardo.rodrigues@softplan.com.br>
2025-07-21 22:10:53 -0300

    ajuste contrato Order

--- src/OrderService.Domain/Entities/Order.cs
+++ src/OrderService.Domain/Entities/Order.cs
@@ -8,7 +8,7 @@ public class Order
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; } = default!;
-
+    public string? Justification { get; set; }
     public string CustomerName { get; set; } = default!;
     public string DeliveryMethod { get; set; } = default!; // ex: "Retirada", "Entrega"
     public List<OrderItem> Items { get; set; } = new();
